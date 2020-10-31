12e5eae45e856fc9642d61c4f6469eb788bb1c12
Nakamuramasa <ebrten14@gmail.com>
2020-11-01 01:31:39 +0900

    first commit

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::group(['middleware' => ['auth:api']], function(){
+
+});
+
+Route::group(['middleware' => ['guest:api']], function (){
+    Route::post('register','Auth\RegisterController@register');
+});
