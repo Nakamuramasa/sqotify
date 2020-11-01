657d90955c55ff88b3967c07fa074361f33cd4f6
Nakamuramasa <ebrten14@gmail.com>
2020-11-02 00:49:26 +0900

    Created authentication methods

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -2,32 +2,15 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Verified;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\VerifiesEmails;
 
 class VerificationController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Email Verification Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling email verification for any
-    | user that recently registered with the application. Emails may also
-    | be re-sent if the user didn't receive the original email message.
-    |
-    */
-
-    use VerifiesEmails;
-
-    /**
-     * Where to redirect users after verification.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
     /**
      * Create a new controller instance.
      *
@@ -35,8 +18,50 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
-        $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    public function verify(Request $request, User $user)
+    {
+        if(! URL::hasValidSignature($request)){
+            return response()->json(["errors" => [
+                "message" => "URLの有効期限が切れています。"
+            ]], 422);
+        }
+
+        if($user->hasVerifiedEmail()){
+            return response()->json(["errors" => [
+                "message" => "メールアドレスは既に有効化されています。"
+            ]], 422);
+        }
+
+        $user->markEmailAsVerified();
+        event(new Verified($user));
+
+        return response()->json(['message' => 'メールアドレスが有効化されました。'], 200);
+    }
+
+    public function resend(Request $request)
+    {
+        $this->validate($request, [
+            'email' => ['email', 'required']
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if(! $user){
+            return response()->json(["errors" => [
+                "email" => "このメールアドレスを使用するユーザーが見つかりませんでした。"
+            ]], 422);
+        }
+
+        if($user->hasVerifiedEmail()){
+            return response()->json(["errors" => [
+                "message" => "メールアドレスは既に有効化されています。"
+            ]], 422);
+        }
+
+        $user->sendEmailVerificationNotification();
+
+        return response()->json(['status' => "新しいURLを送信しました。"]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
+Route::get('me', 'User\MeController@getMe');
 
 Route::group(['middleware' => ['auth:api']], function(){
-
+    Route::post('logout', 'Auth\LoginController@logout');
 });
 
 Route::group(['middleware' => ['guest:api']], function (){
     Route::post('register','Auth\RegisterController@register');
+    Route::post('verification/verify/{user}','Auth\VerificationController@verify')->name('verification.verify');
+    Route::post('verification/resend','Auth\VerificationController@resend');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 });
